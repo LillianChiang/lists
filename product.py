81225265862bdbd505af38ec07de9eaf226cd852
LillianChiang <45815283+LillianChiang@users.noreply.github.com>
2021-07-02 09:48:50 +1000

    Add files via upload

--- product.py
+++ product.py
@@ -1,3 +1,12 @@
+#read files
+products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if 'name, price' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+
 
 products = []
 while True:
@@ -5,13 +14,20 @@ while True:
     if name == 'q': #quit
         break
     price = input('please enter the price: ')
+    price = int(price)
     p = (name , price)
-    products.append(p)
+    products.append([name, price])
 print(products) 
 
 for p in products:
-    print(p[0], '$', p[1])
+    print(p[0], 'is $', p[1])
 
-with open('products.txt', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('Name, Price\n')
     for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+        f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+
+
+
