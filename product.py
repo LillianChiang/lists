71384d94d225db596b7158c1f979c30faa2ca2b0
LillianChiang <45815283+LillianChiang@users.noreply.github.com>
2021-07-09 14:34:03 +1000

    Update product.py
    check files before read it

--- product.py
+++ product.py
@@ -1,14 +1,20 @@
-#read files
-products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if 'name, price' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-
+import os # operating sysytem
 
 products = []
+if os.path.isfile('products.csv'):
+    print('yeah! found the file!')
+    #read files
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if 'name, price' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('no files...')
+
+#input
 while True:
     name = input('please enter the name of product: ')
     if name == 'q': #quit
